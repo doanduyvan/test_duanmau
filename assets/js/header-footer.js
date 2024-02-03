fd413c21cbf50659cb76b59ae3bc72de3f75d4f6
doanduyvan <duyvanlee2001@gmail.com>
2024-02-03 11:19:23 +0700

    up

---
+++ assets/js/header-footer.js
@@ -0,0 +1,11 @@
+function Dom(element){
+    return document.querySelector(element);
+}
+function DomALL(element){
+    return document.querySelectorALL(element);
+}
+
+window.addEventListener('load',()=>{
+    
+    
+})
